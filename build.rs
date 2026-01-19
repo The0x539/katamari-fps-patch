01728d71bf2e92b96724d314c49cae58244ad5f7
The0x539 <the0x539@gmail.com>
2026-01-19 09:18:22 -0600

    find the visual studio installation (for the linker)

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
 fn main() -> Result<(), String> {
+    if cfg!(windows) {
+        let mut vcvars = vcvars::Vcvars::new();
+        let path = vcvars.get_cached("PATH").unwrap();
+        unsafe {
+            std::env::set_var("PATH", path.as_ref());
+        }
+    }
+
     nasm_rs::compile_library(
         "native_replacements.lib",
         &["src/replacements/abilities.asm"],
